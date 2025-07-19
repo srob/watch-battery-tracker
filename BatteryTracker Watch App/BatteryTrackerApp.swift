bc5ec486f4df3ef9ffbdc859ef0129f587eb0802
srob <sjroberts@gmail.com>
2025-07-19 13:28:55 +0100

    Initial Commit

---
+++ BatteryTracker Watch App/BatteryTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryTrackerApp.swift
+//  BatteryTracker Watch App
+//
+//  Created by Simon Roberts on 19/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct BatteryTracker_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BatteryTracker Watch AppTests/BatteryTracker_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryTracker_Watch_AppTests.swift
+//  BatteryTracker Watch AppTests
+//
+//  Created by Simon Roberts on 19/07/2025.
+//
+
+import Testing
+@testable import BatteryTracker_Watch_App
+
+struct BatteryTracker_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
